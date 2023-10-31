9f1c10413d407ad3974a13310772343b89976049
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-10-31 14:43:29 +0300

    Spring Boot configuration pt1

---
+++ src/main/java/com/magidovtimofey/spring/springboot/spring_springboot/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package com.magidovtimofey.spring.springboot.spring_springboot.dao;
+
+
+
+
+
+import com.magidovtimofey.spring.springboot.spring_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//
+//    public void deleteEmployee(int id);
+}
